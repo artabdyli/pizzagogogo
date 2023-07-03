33b36fbeb150e56e010bad93ed4ea6ffd8dbc529
Art <116184113+artabdyli@users.noreply.github.com>
2023-07-03 09:03:02 +0200

    PC to Laptop Commit
    new City, Delivery and Driving done

--- Assets/Assets/ThirdPersonUnity/InputSystem/StarterAssetsInputs.cs
+++ Assets/Assets/ThirdPersonUnity/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool drive;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,6 +44,11 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnDrive(InputValue value)
+		{
+			DriveInput(value.isPressed);
+		}
 #endif
 
 
@@ -64,6 +70,10 @@ namespace StarterAssets
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
+		}
+		public void DriveInput(bool newDriveState)
+		{
+			drive = newDriveState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
---
+++ Assets/Scripts/DriveCar.cs
@@ -0,0 +1,62 @@
+using StarterAssets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class DriveCar : MonoBehaviour
+{
+    private StarterAssetsInputs playerInput;
+    private StarterAssetsInputs input;
+    [SerializeField] private GameObject player;
+    private CarController carController;
+    private PlayerInput carInput;
+    [SerializeField] private GameObject carFollowCam;
+
+
+    private void Start()
+    {
+        carController = GetComponent<CarController>();
+        input = GetComponent<StarterAssetsInputs>();
+        carInput = GetComponent<PlayerInput>();
+        playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<StarterAssetsInputs>();
+    }
+    private void Update()
+    {
+        if(input.drive && carController.enabled)
+        {
+            input.drive = false;
+            GetOut();
+            
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player") && playerInput.drive)
+        {
+            playerInput.drive = false;
+            Debug.Log("Get in the Car");
+            GetIn();
+        }
+    }
+
+    private void GetIn()
+    {
+        player.SetActive(false);
+        carController.enabled = true;
+        carInput.enabled = true;
+        carFollowCam.SetActive(true);
+
+    }
+
+    private void GetOut()
+    {
+        carController.enabled = false;
+        carInput.enabled = false;
+        carFollowCam.SetActive(false);
+        player.SetActive(true);
+        player.transform.position = new Vector3(this.transform.position.x + 2, this.transform.position.y, this.transform.position.z);
+    }
+}
